bf7659f19fd0749b45e28a1c1c940567bd554807
jetilling <jetilling@hotmail.com>
2016-12-09 10:59:14 -0700

    added jwt authentication

--- index.js
+++ index.js
@@ -17,11 +17,38 @@ var app = module.exports = express();
 app.set('db', db);
 
 //----Server Controllers----//
+var authCtrl = require('./controllers/authCtrl.js');
 
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/public'));
+
+//----Login Required Middleware----//
+function ensureAuthenticated(req, res, next) {
+  if (!req.header('Authorization')) {
+    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+  }
+  var token = req.header('Authorization').split(' ')[1];
+  var payload = null;
+  try {
+    payload = jwt.decode(token, config.TOKEN_SECRET);
+  }
+  catch (err) {
+    return res.status(401).send({ message: err.message });
+  }
+  if (payload.exp <= moment().unix()) {
+    return res.status(401).send({ message: 'Token has expired' });
+  }
+  req.user = payload.sub;
+  next();
+}
+
+//----Endpoints----//
+//----authCtrl----//
+app.get('/api/me', ensureAuthenticated, authCtrl.getMe);
+app.post('/auth/login', authCtrl.login);
+app.post('/auth/signup', authCtrl.signUp);
 
 var port = config.port
 app.listen(port, function(){
--- public/views/login/loginCtrl.js
+++ public/views/login/loginCtrl.js
@@ -1,3 +1,19 @@
-angular.module('candle').controller('loginCtrl', function($scope, $state){
+angular.module('candle').controller('loginCtrl', function($scope, $state, $auth){
+
+  //login function - response.data.token accesses token.
+  $scope.logIn = function(email, password) {
+    $auth.login({
+      email: email,
+      password: password,
+    }).then(function (response) {
+      console.log("loginCtrl:", response);
+      if(response.status === 200){
+        $auth.setToken(response);
+        $state.go('profile');
+      }
+    }).catch(function (response) {
+      console.log("loginCtrl Error:", response);
+    });
+  };
 
 })
