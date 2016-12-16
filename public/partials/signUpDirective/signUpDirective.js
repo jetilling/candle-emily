59d0e7ac31edac8085f742d1f05b18473b8a2d24
jetilling <jetilling@hotmail.com>
2016-12-16 15:18:43 -0700

    modals move with scroll

--- public/partials/signUpDirective/signUpDirective.js
+++ public/partials/signUpDirective/signUpDirective.js
@@ -5,38 +5,47 @@ angular.module('candle').directive('signUpDirective', function(){
     controller: function($scope, mainService, $auth){
 
       $scope.mismatchedPasswords = false;
-        $scope.existingEmail = false;
+      $scope.existingEmail = false;
 
-        $scope.create = function(firstName, lastName, email, password, confirmPassword){
-          if (password === confirmPassword){
-            $auth.signup({
-              firstName: firstName,
-              lastName: lastName,
-              password: password,
-              email: email
-            }).then(function (response) {
-              console.log("signUpCtrl:", response);
-              $auth.setToken(response);
-              $scope.signUpDirective = false;
-              if ($auth.isAuthenticated()){
-                mainService.userData()
-                .then(function(response){
-                  console.log(response[0])
-                  $scope.name = response[0].first_name
-                })
-                $scope.accountName = true;
-                $scope.logoutBtn = true;
-                $scope.loginBtn = false;
-                $scope.signUpBtn = false;
+      $scope.closeModal = function(){
+        $scope.signUpDirective = false;
+      }
 
-              }
-            }).catch(function (response) {
-              console.log("signUpCtrl Error:", response);
-              $scope.existingEmail = true
-            });
-          }
-          else $scope.mismatchedPasswords = true
+      $scope.switchToLogin = function(){
+        $scope.signUpDirective = false;
+        $scope.loginDirective = true;
+      }
+
+      $scope.create = function(firstName, lastName, email, password, confirmPassword){
+        if (password === confirmPassword){
+          $auth.signup({
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            email: email
+          }).then(function (response) {
+            console.log("signUpCtrl:", response);
+            $auth.setToken(response);
+            $scope.signUpDirective = false;
+            if ($auth.isAuthenticated()){
+              mainService.userData()
+              .then(function(response){
+                console.log(response[0])
+                $scope.name = response[0].first_name
+              })
+              $scope.accountName = true;
+              $scope.logoutBtn = true;
+              $scope.loginBtn = false;
+              $scope.signUpBtn = false;
+
+            }
+          }).catch(function (response) {
+            console.log("signUpCtrl Error:", response);
+            $scope.existingEmail = true
+          });
         }
+        else $scope.mismatchedPasswords = true
+      }
 
     }
   }
